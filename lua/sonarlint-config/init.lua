b5360114c7ec7110a76f75fe9e265de423ca0fd5
antonio <antoniorodriguezruiz2002@gmail.com>
2023-10-02 14:07:12 +0200

    Added sonarlint
    Original code by "https://gitlab.com/schrieveslaach/sonarlint.nvim". Not using it through Packer because the setup gives some weird error that does not occur by just copying the code

---
+++ lua/sonarlint-config/init.lua
@@ -0,0 +1,24 @@
+local home = os.getenv("HOME")
+MASON_PTH = home .. "/.local/share/nvim/mason/packages/sonarlint-language-server"
+
+require("sonarlint-config.sonarlint").setup({
+	server = {
+		cmd = {
+			MASON_PTH .. "$MASON/sonarlint-language-server",
+			-- Ensure that sonarlint-language-server uses stdio channel
+			"-stdio",
+			"-analyzers",
+			-- paths to the analyzers you need, using those for python and java in this example
+			vim.fn.expand(MASON_PTH .. "/extension/analyzers/sonarpython.jar"),
+			vim.fn.expand(MASON_PTH .. "/extension/analyzers/sonarcfamily.jar"),
+			vim.fn.expand(MASON_PTH .. "/extension/analyzers/sonarjava.jar"),
+		},
+	},
+	filetypes = {
+		-- Tested and working
+		"python",
+		"cpp",
+		-- Requires nvim-jdtls, otherwise an error message will be printed
+		"java",
+	},
+})
